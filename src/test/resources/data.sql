d8b0b82ad461a7051597d145ee9927cbbc943585
Aleksandr Uvarov <sanyauvarov@mail.ru>
2021-09-03 01:22:00 +0300

    add more init data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
-insert into author (name) values ('Pushkin');
-insert into genre (name) values ('novel');
+insert into author (name) values ('Pushkin'), ('Brodsky');
+insert into genre (name) values ('novel'), ('fantasy');
 insert into book (title, author_id, genre_id) values ('Eugene Onegin', 0, 0);
